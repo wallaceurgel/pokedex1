9088b897860281a9e5f7a945ad4b20cc37f9c739
wallaceurgel <wallaceurgel2021@gmail.com>
2025-11-25 22:06:26 -0300

    adicionando-estilos-botao-info

---
+++ event.js
@@ -0,0 +1,35 @@
+const form = document.querySelector(".form");
+const input = document.querySelector(".inputSearch");
+const buttonPrev = document.querySelector(".btn");
+const buttonNext = document.querySelector(".btn-next");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (input.value <= 721) {
+    pokemonAtual = 1;
+    mostraPokemon(input.value.toLowerCase());
+  } else {
+    pokemonAtual = 1;
+    alert("a pokedex possue apenas 721 pokemons");
+    mostraPokemon(pokemonAtual);
+  }
+  input.value = "";
+});
+buttonPrev.addEventListener("click", () => {
+  if (pokemonAtual > 1) {
+    pokemonAtual -= 1;
+    mostraPokemon(pokemonAtual);
+  } else {
+    mostraPokemon(pokemonAtual);
+  }
+});
+buttonNext.addEventListener("click", () => {
+  if (pokemonAtual >= 721) {
+    pokemonAtual = 1;
+    alert("a pokedex possue apenas 721 pokemons");
+  } else {
+    pokemonAtual += 1;
+  }
+  mostraPokemon(pokemonAtual);
+});
---
+++ script.js
@@ -0,0 +1,42 @@
+const pokemonName = document.querySelector(".pokemonNome");
+const pokemonNumber = document.querySelector(".pokemonNum");
+const pokemonImage = document.querySelector(".pokemonImg");
+//const input = document.querySelector(".inputSearch"); tentando fazer com que o input >721 volte para o pokemon 1
+
+let pokemonAtual;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+  const data = await APIResponse.json();
+
+  return data;
+};
+
+const mostraPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+
+  if (data.id < 650) {
+    pokemonAtual = data.id;
+    pokemonImage.src =
+      data[`sprites`][`versions`][`generation-v`][`black-white`][`animated`][
+        `front_default`
+      ];
+  } else {
+    pokemonImage.src =
+      data[`sprites`][`versions`][`generation-vi`][`omegaruby-alphasapphire`][
+        `front_default`
+      ];
+    pokemonAtual = data.id;
+  }
+  if (data.id > 721 /* && input > 720*/) {
+    data.id = 0;
+    pokemonAtual = data.id;
+  } else {
+    pokemonAtual = data.id;
+  }
+};
+mostraPokemon(1);
